c015f4fbf0de20b0c57bba629cbb8387d0a74142
Alexander Kozlenko <alexander.kozlenko@live.com>
2017-02-11 23:21:56 +0200

    Add unit test with JsonSerializer instance and custom converter

--- src/System.Data.JsonRpc.Tests/JsonRpcSerializerTests.cs
+++ src/System.Data.JsonRpc.Tests/JsonRpcSerializerTests.cs
@@ -361,6 +361,43 @@ namespace System.Data.JsonRpc.Tests
             Assert.True(jsonRpcMessageInfo.Success);
         }
 
+        [Fact]
+        public void DeserializeRequestDataWithCustomJsonConverter()
+        {
+            var jsonRpcSchema = new JsonRpcSchema();
+
+            jsonRpcSchema.SupportedMethods.Add("test_method");
+            jsonRpcSchema.ParameterTypeBindings["test_method"] = typeof(TestParams);
+
+            var jsonSerializer = JsonSerializer.CreateDefault();
+
+            jsonSerializer.Converters.Add(new TestJsonConverter());
+
+            var jsonRpcSettings = new JsonRpcSettings();
+
+            jsonRpcSettings.JsonSerializer = jsonSerializer;
+
+            var jsonRpcSerializer = new JsonRpcSerializer(jsonRpcSchema, jsonRpcSettings);
+            var jsonSample = JsonTools.GetJsonSample("jrcc_01_req");
+            var jsonRpcDataInfo = jsonRpcSerializer.DeserializeRequestsData(jsonSample);
+
+            Assert.False(jsonRpcDataInfo.IsEmpty);
+            Assert.False(jsonRpcDataInfo.IsBatch);
+
+            var jsonRpcMessageInfo = jsonRpcDataInfo.GetItem();
+
+            Assert.NotNull(jsonRpcMessageInfo);
+            Assert.True(jsonRpcMessageInfo.Success);
+
+            var jsonRpcRequest = jsonRpcMessageInfo.GetItem();
+
+            Assert.IsType<TestParams>(jsonRpcRequest.Params);
+
+            var jsonRpcRequestParams = (TestParams)jsonRpcRequest.Params;
+
+            Assert.Equal("value", jsonRpcRequestParams.Value);
+        }
+
         #region Test Types
 
         private sealed class TestJsonArrayPool : IArrayPool<char>
@@ -370,6 +407,25 @@ namespace System.Data.JsonRpc.Tests
 
             public void Return(char[] array) =>
                 ArrayPool<char>.Shared.Return(array);
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        private sealed class TestParams
+        {
+            [JsonProperty("value")]
+            public string Value { get; set; }
+        }
+
+        private sealed class TestJsonConverter : JsonConverter
+        {
+            public override bool CanConvert(Type objectType) =>
+                objectType == typeof(string);
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) =>
+                (string)reader.Value == "default_value" ? "value" : reader.Value;
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) =>
+                throw new NotImplementedException();
         }
 
         #endregion
